7bfc5847a59c57662014272fdec85e310947fc15
boldhuu <d.boldkhuu@gmail.com>
2015-10-19 14:48:46 -0500

    Added user & Payment repo

---
+++ src/main/java/com/pettypal/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.pettypal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pettypal.domain.Payment;;
+
+@Repository
+public interface PaymentRepository extends CrudRepository<Payment, Long> {
+
+}
