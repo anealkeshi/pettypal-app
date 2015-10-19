5f144844e60bcd829aab570e5eedf82f5498e30f
boldhuu <d.boldkhuu@gmail.com>
2015-10-19 11:52:57 -0500

    configured Mysql Depend on pom xml - tested

--- src/main/java/com/pettypal/domain/User.java
+++ src/main/java/com/pettypal/domain/User.java
@@ -1,5 +1,25 @@
 package com.pettypal.domain;
 
-public class User {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name="user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	private String firstName;
+	private String lastName;
+	
+	private String phonenumber;
+	private String email;
+	
+	private Date birthDate;
+	
 }
