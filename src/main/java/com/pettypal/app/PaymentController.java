8a80da2ebf94a0a79589bc6e67fccb0c1c2b0702
Michael <mickeyan@gmail.com>
2015-10-21 17:29:56 -0500

    Fixed missing annotation.

--- src/main/java/com/pettypal/app/PaymentController.java
+++ src/main/java/com/pettypal/app/PaymentController.java
@@ -6,6 +6,7 @@ import java.security.Principal;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +28,7 @@ import com.pettypal.service.PaymentService;
 //@SessionAttributes("savedPayment")
 public class PaymentController {
 	
+	@Autowired
 	private PaymentService paymentService;
 	
 	@RequestMapping(value = {"","/request"}, method = RequestMethod.GET)
