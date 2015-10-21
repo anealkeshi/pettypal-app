a3d61460f1bfbe81ca1ded943c50fb251083cbae
Anil <anilksee@gmail.com>
2015-10-20 20:24:13 -0500

    Added one more user

--- src/main/resources/scripts/populate.sql
+++ src/main/resources/scripts/populate.sql
@@ -1,2 +1,5 @@
-INSERT INTO credential(created_at,created_by,updated_at,updated_by,id,password,username) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,1,'password','user');
-INSERT INTO user(created_at,created_by,updated_at,updated_by,id,firstName,lastName,phonenumber,credential_id) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,1,'Anil', 'K C', '6419808654', 1);
+INSERT INTO credential(created_at,created_by,updated_at,updated_by,password,username) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'password','user');
+INSERT INTO credential(created_at,created_by,updated_at,updated_by,password,username) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'admin','admin');
+
+INSERT INTO user(created_at,created_by,updated_at,updated_by,firstName,lastName,phonenumber,credential_id) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'Anil', 'K C', '6419808654', 1);
+INSERT INTO user(created_at,created_by,updated_at,updated_by,firstName,lastName,phonenumber,credential_id) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'Boldkhuu', 'Dadsas', '6419808654', 2);
