e6f25e36796e27ce3c7db5cd67adf6aac49e60eb
Anil <anilksee@gmail.com>
2015-10-21 23:31:52 -0500

    Added testuser

--- src/main/resources/scripts/populate.sql
+++ src/main/resources/scripts/populate.sql
@@ -1,4 +1,4 @@
-INSERT INTO credential(created_at,created_by,updated_at,updated_by,password,username) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'password','user');
+INSERT INTO credential(created_at,created_by,updated_at,updated_by,password,username) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'password','testuser');
 INSERT INTO credential(created_at,created_by,updated_at,updated_by,password,username) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'admin','admin');
 
 INSERT INTO user(created_at,created_by,updated_at,updated_by,firstName,lastName,phonenumber,credential_id,email,birthDate) values(CURRENT_TIMESTAMP,1,CURRENT_TIMESTAMP,1,'Anil', 'K C', '641-9808-654', 1,'akc@mum.edu',CURRENT_TIMESTAMP);
